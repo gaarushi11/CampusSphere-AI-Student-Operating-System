123f9087915e1ffb1c288bad7e046dfb33d7623e
Sumit Sharma <sharmasumit0118@gmail.com>
2026-06-14 10:35:15 +0530

    rag1

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['pdf-parse', 'mammoth'],
+  serverComponentsExternalPackages: ['pdf-parse', 'mammoth'],
 };
 
 export default nextConfig;
